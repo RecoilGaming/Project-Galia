eae91f4b0460de062ba0a5e8346deb12477631b0
TheGrapeApe22 <rnadkarni221@student.fuhsd.org>
2024-12-27 13:04:40 -0800

    added button and shop window

--- %Project/%Scripts/shop_ui.gd
+++ %Project/%Scripts/shop_ui.gd
@@ -1,15 +1,7 @@
-extends Node2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+extends Control
 
 func _on_h_button_pressed() -> void:
-	print("hello")
-	pass
+	print("frick you")
+
+func _on_back_button_pressed() -> void:
+	queue_free()
