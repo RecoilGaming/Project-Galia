00c8a291ee9c581e3ec68096bdeea94260c2e94d
TheGrapeApe22 <rnadkarni221@student.fuhsd.org>
2024-12-27 14:52:38 -0800

    add unit selection

--- %Project/%Scripts/select_unit.gd
+++ %Project/%Scripts/select_unit.gd
@@ -1,13 +1,16 @@
-extends Button
+extends Control
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _on_pressed() -> void:
-	get_parent().add_child(load("res://%Project/%Levels/shop.tscn").instantiate())
-	self.hide()
+
+func _on_back_button_pressed() -> void:
+	
+	pass # Replace with function body.
