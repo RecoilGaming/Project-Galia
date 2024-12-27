e0e7c940c93cbf0f78841b3a580aaca26db51122
Dungewar <90892883+Dungewar@users.noreply.github.com>
2024-12-27 12:15:28 -0800

    Fix unit movement

--- %Project/%Scripts/Components/unit.gd
+++ %Project/%Scripts/Components/unit.gd
@@ -13,6 +13,7 @@ var target: Unit
 
 # Ready
 func _ready() -> void:
+	# Adds a unit to the list of all units in the GameManager
 	GM.add_unit(self)
 
 # Process
@@ -22,14 +23,11 @@ func _process(delta: float) -> void:
 	
 	# Track target
 	if target:
-		var _position = Vector2(
-			move_toward(global_position.x, target.global_position.x, speed * delta),
-			move_toward(global_position.y, target.global_position.y, speed * delta)
-		)
+		var _position = global_position.move_toward(target.global_position, speed*delta)
 		
 		global_position = _position
 
-# Find target
+# Finds the nearest target
 func find_target() -> void:
 	# Minimum distance
 	var min_dist: float = 1000000
