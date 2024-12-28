fb1b27a678ca03ea48a42cce0b4e0ad90b86e1f2
TheGrapeApe22 <rnadkarni221@student.fuhsd.org>
2024-12-27 16:32:08 -0800

    add polarity button

--- %Project/%Scripts/Units/hydrogen.gd
+++ %Project/%Scripts/Units/hydrogen.gd
@@ -3,4 +3,4 @@ class_name Hydrogen
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-		pass
+		change_polarity(1)
