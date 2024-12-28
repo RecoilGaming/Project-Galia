c8ad5948e73867ff76677daf351e92a0f4d4da33
TheGrapeApe22 <rnadkarni221@student.fuhsd.org>
2024-12-28 15:41:38 -0800

    Merge branch 'main' of https://github.com/RecoilGaming/Project-Galia

--- %Project/%Scripts/%Managers/game_manager.gd
+++ %Project/%Scripts/%Managers/game_manager.gd
@@ -6,14 +6,29 @@ extends Node
 @onready var main: Node2D = get_tree().root.get_node("Main")
 
 # Units
-var coins: int = 100
+var coins: int = 100:
+	set(value):
+		coins = value
+		update_coins()
 var units: Array[Unit] = []
 var unit_spawn_index: int = 0
 var types = ["hydrogen", "oxygen", "fluorine"]
 var polarizing: bool = false
 
+var current_wave := 1
+var waves = [
+	[["hydrogen", 5]],
+	[["hydrogen", 3], ["oxygen", 2]],
+	[["hydrogen", 5], ["oxygen", 4]],
+	[["hydrogen", 3], ["oxygen", 2], ["fluorine", 2]],
+	[["hydrogen", 3], ["oxygen", 0], ["fluorine", 5]],
+]
+
+var waiting_for_continue := false
+
 func _ready():
 	update_coins()
+	new_wave()
 
 ## =============== [ METHODS ] ================
 
@@ -24,16 +39,37 @@ func add_unit(unit: Unit):
 # Spawn unit
 func spawn_unit(pos: Vector2, index: int):
 	if coins >= 3:
-		# Instantiate unit
-		var unit: Unit = load("res://%Project/Characters/" + types[index] + ".tscn").instantiate()
-		unit.position = pos
-		
-		# Add child
-		main.add_child(unit)
-		
-		# Subtract cost
-		coins -= 3
-		update_coins()
+		if(try_to_spawn(types[index], pos)):
+			# Subtract cost
+			coins -= 3
 
 func update_coins():
 	main.get_node("CoinText").text = "Coins: " + str(coins)
+
+func try_to_spawn(u: String, pos: Vector2) -> bool:
+	for unit in units:
+		var distance = unit.global_position.distance_to(pos)
+		if(distance < 30):
+			return false
+
+	# Instantiate unit
+	var unit: Unit = load("res://%Project/Characters/" + u + ".tscn").instantiate()
+	unit.position = pos
+	
+	# Add child
+	main.add_child.call_deferred(unit)
+	return true
+
+func new_wave():
+	Engine.set_time_scale(0)
+	waiting_for_continue = true
+	var wave_to_spawn = waves[current_wave]
+	
+	for spawn in wave_to_spawn:
+		var unit_class = spawn[0]
+		var i := 0
+		while i < spawn[1]:
+			if(try_to_spawn(unit_class, Vector2(randi_range(30, 290), randi_range(-150, 150)))): # -320, -180 to 320, 180 is the canvas
+				i += 1
+				
+	current_wave += 1
