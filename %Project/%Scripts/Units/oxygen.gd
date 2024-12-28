84bc8b992fddd673bca2708b3fa243c1c2e4bf8c
Dungewar <dungewar@gmail.com>
2024-12-27 16:47:49 -0800

    Make backing slower

--- %Project/%Scripts/Units/oxygen.gd
+++ %Project/%Scripts/Units/oxygen.gd
@@ -4,11 +4,12 @@ class_name Oxygen
 @export var SHOOT_DAMAGE = 0
 @export var FOLLOWING_DISTANCE = 300
 const FOLLOWING_MARGINS = 30
+const BACKUP_SLOWDOWN = 0.2
 
 func move(dir: Vector2, delta: float):
 	if(global_position.distance_to(target.global_position) < FOLLOWING_DISTANCE - FOLLOWING_MARGINS):
-		super.move(-dir, delta)
-		print("backing up to " + str(-dir))
+		super.move(-dir, delta*BACKUP_SLOWDOWN)
+		print("backing up to " + str(-dir*BACKUP_SLOWDOWN))
 	elif(global_position.distance_to(target.global_position) < FOLLOWING_DISTANCE):
 		velocity = Vector2(0, 0)
 	elif(global_position.distance_to(target.global_position) > (FOLLOWING_DISTANCE + FOLLOWING_MARGINS)):
