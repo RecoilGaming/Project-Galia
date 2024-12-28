96981093c30bb738da3df9d12b1bdabf57384112
TheGrapeApe22 <rnadkarni221@student.fuhsd.org>
2024-12-28 10:48:15 -0800

    Move scripts into UI folder, fix unit adding

--- %Project/%Scripts/UI/shop_ui.gd
+++ %Project/%Scripts/UI/shop_ui.gd
@@ -1,17 +1,13 @@
 extends Control
 
 func _on_h_button_pressed() -> void:
-	if GM.unit_spawn_index == 0:
-		GM.spawn_hydrogen(Vector2(0, 0))
-	else:
-		print(GM.unit_spawn_index)
+	GM.spawn_unit(Vector2(0, 0))
 
 func _on_back_button_pressed() -> void:
 	queue_free()
 
 func _on_polarize_button_pressed() -> void:
 	get_parent().add_child(load("res://%Project/%Levels/select_unit.tscn").instantiate())
-
 
 func _on_option_button_item_selected(index: int) -> void:
 	GM.unit_spawn_index = index
