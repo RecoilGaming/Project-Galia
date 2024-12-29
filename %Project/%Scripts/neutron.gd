4ec830dc0eaf10a28467e60c8b8a2ed372381a8f
Dungewar <dungewar@gmail.com>
2024-12-29 10:58:39 -0800

    Do fix

--- %Project/%Scripts/neutron.gd
+++ %Project/%Scripts/neutron.gd
@@ -7,17 +7,6 @@ func _ready() -> void:
 	KNOCKBACK = 0
 	$Sprite.play("default")
 
-func set_polarity(value):
-	var old_polarity = polarity
-	
-	polarity = clamp(value, Polarity.BIG_POSITIVE, Polarity.BIG_NEGATIVE)
-	
-	if(old_polarity == polarity):
-		return false
-	else:
-		# Changes animation
-		return true
-
 func _process(delta: float) -> void:
 	super(delta)
 	print("DEALING " + str(CONTACT_DAMAGE))
