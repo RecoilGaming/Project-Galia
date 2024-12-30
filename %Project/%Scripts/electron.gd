c3436455963a595a0c998ca948bc500def003a39
Dungewar <dungewar@gmail.com>
2024-12-29 16:00:45 -0800

    Change speed of animatinon

--- %Project/%Scripts/electron.gd
+++ %Project/%Scripts/electron.gd
@@ -1,7 +1,12 @@
 extends Nuetron
 class_name Electron
 
-var polarity
+var polarity:
+	set(value):
+		value = clamp(value, -1, 1)
+		if(value == -1 || value == 1):
+			polarity = value
+			$Sprite.play(str(polarity*2+2))
 
 func _on_not_area_2d_body_entered(body: Node2D) -> void:
 	if(is_valid_target(body)):
