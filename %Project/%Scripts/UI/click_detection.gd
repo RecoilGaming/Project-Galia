d691c0c0d2614f009b50ea78df60565f43eaf437
TheGrapeApe22 <rnadkarni221@student.fuhsd.org>
2024-12-29 19:15:04 -0800

    Make even even more mobile friendly

--- %Project/%Scripts/UI/click_detection.gd
+++ %Project/%Scripts/UI/click_detection.gd
@@ -6,7 +6,6 @@ func _ready():
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
 	if (
 		event is InputEventMouseButton
-		and event.button_index == MOUSE_BUTTON_LEFT
 		and event.pressed
 		and !event.canceled
 		and GM.is_summoning
